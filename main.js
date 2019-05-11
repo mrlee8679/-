a19f9a5ec42001455e8e71e68c31f2de81f05aaa
mrlee8679 <bleach111314@gmail.com>
2019-05-11 22:35:23 +0800

    第一次写无缝轮播代码

---
+++ main.js
@@ -0,0 +1,47 @@
+let n 
+初始化()
+setInterval(() => {
+  makeLeave(getImage(n))
+    .one('transitionend',(e) =>{
+      makeEnter($(e.currentTarget))
+    })
+  makeCurrent(getImage(n+1))
+  n += 1
+},2000)
+
+function 初始化(){
+  n = 1 
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+function x(n){
+  if(n>4){
+    n = n%4
+    if(n === 0){
+      n = 4
+    }
+  }
+  return n 
+}
+
+
+
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node){
+  return $node.removeClass('enter current').addClass('leave')
+}
+
+function makeEnter($node){
+  return $node.removeClass('leave current').addClass('enter')
+}
+
+
+
+
